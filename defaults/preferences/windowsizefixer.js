b3c0b98423a42386a3f8ed083ae7b5d42238156e
YUKI Hiroshi <shimoda@clear-code.com>
2013-10-18 12:32:17 +0900

    Add default preferencest

---
+++ defaults/preferences/windowsizefixer.js
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// "WWWxHHH", "WWW,HHH", "WWW HHH", etc.
+pref("extensions.windowsizefixer@clear-code.com.fixedSize", "800x600");
+pref("extensions.windowsizefixer@clear-code.com.fixOnStartup", true);
+// ex.
+//  Ctrl(Cmd)-F2 => keyCode="VK_F2", key="", modifiers="accel"
+//  Ctrl-Shift-G => keyCode="", key="g", modifiers="accel,shift" or "control,shift"
+pref("extensions.windowsizefixer@clear-code.com.shortcut.keyCode", "");
+pref("extensions.windowsizefixer@clear-code.com.shortcut.key", "f");
+pref("extensions.windowsizefixer@clear-code.com.shortcut.modifiers", "accel,shift");
